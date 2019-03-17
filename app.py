72de6a5db4515e9ed3d8466486326e2aa25a5617
Ziik <ziik@Ziiks-MacBook-Pro.local>
2019-03-17 16:09:33 +0700

    Python String Method and Input

---
+++ app.py
@@ -0,0 +1,46 @@
+# print("Hello World")
+# print('o----')
+# print(' |||| ')
+# print('*'*10)
+#
+# price = 10
+# price = 20
+# rating = 4.9
+# name = 'HuyZiik'
+# is_handsome = False
+# print(price*rating)
+
+# full_name = "John Smith"
+# age = 20
+# is_new = True
+
+# # Input
+# name = input('What is Your Name ?')
+# print(name)
+#
+# birht_year = input('Birth Year: ')
+# print(type(birht_year))
+# age = 2019 - int(birht_year)
+# print(age)
+#
+# course = "Python 's \" for Beginners"
+# course_mail = '''
+#  Hi Huy
+#  Thank for supporting us
+# '''
+# print(course_mail)
+# print(course[0:4])
+#
+# first_name = "John"
+# last_name = "Smith"
+# message = first_name + ' [' + last_name +' ] is a coder'
+# msg = f'{first_name}[{last_name}] is a coder'
+# print(msg)
+
+course = "Python for Beginners" 
+print(len(course))
+print(course.upper())
+print(course.lower())
+print(course.find('P'))
+print(course.replace('Beginners','Pro'))
+print('Python' in course)
